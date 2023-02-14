cd25cea712a245a993c08e3d9f87ef8d6240803c
Melina Señoráns Pérez <melina.senorans@gmail.com>
2023-02-14 19:20:11 -0300

    poniendo de vuelta Redis

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.set('views', './views/')
  app.engine("hbs", hbs);
  app.set("view engine", "hbs");
 
-/*
+
 const redis = require("redis");
 const client = redis.createClient({
   legacyMode: true,
@@ -73,7 +73,7 @@ client.connect()
 .then(() => logger.log("info", "Connected to Redis"))
 .catch((e) => logger.log("error", e));
 const RedisStore = require("connect-redis")(session);
-*/
+
 
 
 //MONGO para models Usuarios y Productos
@@ -164,7 +164,7 @@ passport.deserializeUser((id, done) => {
 });
 
 //SESSION
-/*
+
 app.use(
   session({
     store: new RedisStore({ host: "0.0.0.0", port: 6379, client, ttl: 300 }),
@@ -179,7 +179,7 @@ app.use(
     saveUninitialized: false,
   })
 );
-*/
+
 
 app.use(passport.initialize());
 app.use(passport.session());
